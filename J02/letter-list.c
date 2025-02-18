815a349192ecbb2ad466e216725d1cf183cdf794
Z-清蒸黄鱼 <rantywang@wc124-dhcp-208.brynmawr.edu>
2025-02-18 16:01:53 -0500

    J02 complete

--- J02/letter-list.c
+++ J02/letter-list.c
@@ -1,4 +1,46 @@
-int main()
-{
+# include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+  char c;
+  struct node* next;
+}; 
+
+struct node* insert_first(char character, struct node* previousC) {
+    struct node* newNode = malloc(sizeof(struct node));
+    newNode->c = character;
+    newNode->next = previousC;
+    return newNode;
+}
+
+int main() {
+  printf("Enter a word:");
+  char word[31];
+  scanf("%s", word);
+  char removeC;
+  printf("Enter a character remove:");
+  scanf(" %c", &removeC);
+
+  struct node *list;
+  list = NULL;
+
+  for (int i = strlen(word) - 1 ; i > -1; i--) {
+    if (word[i] != removeC) {
+      list = insert_first(word[i], list);
+    }
+  }
+
+  for(struct node * n = list; n!= NULL; n = n->next) {
+    printf("%c",n->c);
+  }
+  struct node* curr = list;
+  struct node* next = NULL;
+  while(curr != NULL) {
+    next = curr->next;
+    free(curr);
+    curr = next;
+  }
   return 0;
+  printf("/n");
 }
--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,32 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  char word[31];
+  printf("%s", "Enter a string: ");
+  scanf("%s", word);
+  int wordLength = strlen(word);
+  char* arr = malloc(sizeof(char) * wordLength * wordLength);
+  for (int i = 0; i < wordLength; i++) {
+    for (int j = 0; j < wordLength; j++) {
+      if (i == 0 | i == wordLength - 1) {
+        arr[i * wordLength + j] = word[j];
+      }
+      else if (j == i) {
+        arr[i * wordLength + j] = word[i];
+      }
+      else {
+        arr[i * wordLength + j] = ' ';
+      }
+    }
+  }
+  for (int i = 0; i < wordLength; i++) {
+    for (int j = 0; j < wordLength; j++) {
+      printf("%c ", arr[i * wordLength + j]);
+    }
+    printf("\n");
+  }
+  free(arr);
   return 0;
 }
