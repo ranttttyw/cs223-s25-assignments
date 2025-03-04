11f9c99d2c5f333b4b50b3a32f9f4931a36d71ef
Z-清蒸黄鱼 <rantywang@Z-MacBook-Air.local>
2025-03-04 16:42:34 -0500

    J03 complete

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -1,4 +1,45 @@
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main(int argc, char* argv[]) {
+  if ( argc != 2){
+    printf("Usage: ./hex2int\n");
+    return 0;
+  }
+
+  //if binary
+  if( argv[1][1] == 'b' && argv[1][0] == '0'){
+    int sum = 0;
+    //if 1, shift and add, else do nothing
+    for( int i = strlen(argv[1]) - 3; i >= 0; i--){
+      if( argv[1][strlen(argv[1])-i-1] == '1'){
+        sum += 1 << i;
+        
+      }
+    }
+    printf("%d\n", sum
+    
+    );
+  }
+
+  //if hexadecimal
+  if( argv[1][1] == 'x' && argv[1][0] == '0'){
+    int sum = 0;
+
+    //converting each digit from hexadecimal to decimal
+    for( int i = strlen(argv[1]) - 3; i >= 0; i--){
+      if( argv[1][strlen(argv[1])-i-1] != '0'){
+        //seperating 0-9 from A-F
+        if ( isdigit(argv[1][strlen(argv[1])-i-1])){
+          //binary shifting
+          sum += (argv[1][strlen(argv[1])-i-1]-'0') << (i*4);
+        } else {
+          sum += (argv[1][strlen(argv[1])-i-1]-55) << (i*4);
+        }  
+      }
+    }
+    printf("%d\n", sum);
+  }
   return 0;
 }
--- J03/int2hex.c
+++ J03/int2hex.c
@@ -1,4 +1,44 @@
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char* argv[]) {
+  if ( argc != 2){
+    printf("Usage: ./int2hex\n");
+    return 0;
+  }
+  unsigned int integer = atoi(argv[1]);
+
+  //printing hexadecimal
+  printf("0x");
+  for( int i = 7; i >= 0; i--){
+    if ( (i+1) % 2 == 0){
+      printf(" ");
+    }
+    if(((integer >> (i * 4)) & 15) > 0){
+      printf("%X", ((integer >> (i * 4)) & 15));
+    } else {
+      printf("0");
+    }
+  }
+  printf("\n");
+
+  //printing binary
+  printf("0b");
+  for(int i = 31; i >= 0; i--){
+
+    if ( (i+1) % 4 == 0){
+      printf(" ");
+    }
+
+    if ((integer & (1 << i)) > 0){
+      printf("1");
+    } else {
+      printf("0");
+    }
+  }
+  printf("\n");
+  
   return 0;
 }
