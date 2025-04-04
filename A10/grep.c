e1577fb1d4ba99fd5d5ec85220fc89e74cab65d8
Z-清蒸黄鱼 <rantywang@Z-MacBook-Air.local>
2025-04-04 06:07:11 -0400

    assignment 10 complete

--- A10/grep.c
+++ A10/grep.c
@@ -1,5 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 
-int main() {
+#define MAX_LINE_LEN 1024
+
+typedef struct
+{
+  int thread_id;
+  int num_threads;
+  char *keyword;
+  int num_files;
+  char **files;
+} ThreadArgs;
+
+void *grep_thread(void *arg)
+{
+  ThreadArgs *args = (ThreadArgs *)arg;
+  int id = args->thread_id;
+  int count = 0;
+
+  // Calculate work range
+  int files_per_thread = args->num_files / args->num_threads;
+  int remainder = args->num_files % args->num_threads;
+  int start = id * files_per_thread + (id < remainder ? id : remainder);
+  int end = start + files_per_thread + (id < remainder ? 1 : 0);
+
+  printf("Thread %d searching %d files (%d to %d)\n", id, end - start, start, end - 1);
+
+  for (int i = start; i < end; ++i)
+  {
+    FILE *fp = fopen(args->files[i], "r");
+    if (!fp)
+    {
+      perror(args->files[i]);
+      continue;
+    }
+
+    char line[MAX_LINE_LEN];
+    while (fgets(line, sizeof(line), fp))
+    {
+      if (strstr(line, args->keyword))
+      {
+        printf("%d) %s:%s", id, args->files[i], line);
+        count++;
+      }
+    }
+
+    fclose(fp);
+  }
+
+  // Return count
+  int *result = malloc(sizeof(int));
+  *result = count;
+  pthread_exit(result);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 4)
+  {
+    fprintf(stderr, "usage: %s <NumThreads> <Keyword> <Files>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  int num_threads = atoi(argv[1]);
+  if (num_threads <= 0)
+  {
+    fprintf(stderr, "Error: number of threads must be > 0\n");
+    return EXIT_FAILURE;
+  }
+
+  char *keyword = argv[2];
+  int num_files = argc - 3;
+  char **files = &argv[3];
+
+  pthread_t *threads = malloc(sizeof(pthread_t) * num_threads);
+  ThreadArgs *args = malloc(sizeof(ThreadArgs) * num_threads);
+
+  for (int i = 0; i < num_threads; ++i)
+  {
+    args[i].thread_id = i;
+    args[i].num_threads = num_threads;
+    args[i].keyword = keyword;
+    args[i].num_files = num_files;
+    args[i].files = files;
+
+    pthread_create(&threads[i], NULL, grep_thread, &args[i]);
+  }
+
+  int total_matches = 0;
+  for (int i = 0; i < num_threads; ++i)
+  {
+    void *res;
+    pthread_join(threads[i], &res);
+    total_matches += *(int *)res;
+    free(res);
+  }
+
+  free(threads);
+  free(args);
+
+  printf("Total matches: %d\n", total_matches);
   return 0;
 }
--- A10/marco_polo.c
+++ A10/marco_polo.c
@@ -1,4 +1,75 @@
+/*----------------------------------------------
+ * Author: Ranty Wang
+ * Date: 4/4/2025
+ * Description: Uses signals to coordinate between two processes.
+ ---------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+
+pid_t child_pid = 0;
+pid_t parent_pid = 0;
+
+// Parent's signal handler for SIGALRM
+void parent_handler(int signum)
+{
+  printf("Polo [%d]\n", getpid());
+  fflush(stdout);
+}
+
+// Child's signal handler for SIGALRM
+void child_handler(int signum)
+{
+  printf("Marco [%d]\n", getpid());
+  fflush(stdout);
+
+  // Send SIGALRM back to parent
+  kill(parent_pid, SIGALRM);
+}
+
 int main()
 {
+  parent_pid = getpid();
+  printf("Parent is %d\n", parent_pid);
+  fflush(stdout);
+
+  child_pid = fork();
+
+  if (child_pid < 0)
+  {
+    perror("fork failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (child_pid == 0)
+  {
+    signal(SIGALRM, child_handler);
+    while (1)
+    {
+      pause();
+    }
+  }
+  else
+  {
+    signal(SIGALRM, parent_handler);
+    char input;
+    while (1)
+    {
+      input = getchar();
+
+      if (input == 'm')
+      {
+        kill(child_pid, SIGALRM);
+      }
+      else if (input == 'q')
+      {
+        kill(child_pid, SIGTERM);
+        break;
+      }
+    }
+  }
   return 0;
 }
